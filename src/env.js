6a7dfcc7a92fd643fde1e4875c97eb001c6e39b7
Anruag Parida <anuragparida37@gmail.com>
2022-01-25 21:48:30 +0530

    Env

--- src/env.js
+++ src/env.js
@@ -1,7 +1,7 @@
 const Cookies = require("js-cookie");
 
 module.exports = {
-  server: "http://143.110.181.191/api",
+  server: "http://143.110.181.191:7400/api",
   // server: "localhost:8000/api",
 
   config: {
